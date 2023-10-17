7f3254ccd6365b4506faa0a87bb16bd804f94b3b
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-10-17 17:33:35 +0530

    ✨ feat: add terms of service

--- src/components/LoginSignUpBody/LoginSignUpBody.jsx
+++ src/components/LoginSignUpBody/LoginSignUpBody.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import './LoginSignUpBody.scss';
+import data from './TermsOfService';
 
 const LoginSignUpBody = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isVisiblePolicy, setIsVisiblePolicy] = useState(false);
 
   const toggleLoginRegister = () => {
     setIsLogin(!isLogin);
@@ -25,21 +27,94 @@ const LoginSignUpBody = () => {
         </div>
       ) : (
         <div className="register-section">
+          {
+            isVisiblePolicy &&
+            (
+              <div className='policy' >
+                <div className="policy-content">
+                  <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    position: 'sticky',
+                    top: 0,
+                    background: '#121316',
+                    paddingTop: 20,
+                    paddingBottom: 20,
+                    borderBottom: "1px solid #747476"
+                  }}>
+                    <h4
+                      style={{
+                        margin: 0
+                      }}
+                    >Terms of Service and Privacy Policy</h4>
+                    <div>
+                      <i style={{
+                        fontSize: 22,
+                        cursor: 'pointer'
+                      }} className="fa-solid fa-xmark"
+                        onClick={() => setIsVisiblePolicy(!isVisiblePolicy)}
+                      >
+                      </i>
+                    </div>
+                  </div>
+                  <p>
+                    By using the SIGHT application, you agree to comply with and be bound by these Terms of Service. If you do not agree to these Terms, please do not use the application.
+                  </p>
+                  {
+                    data.map((item, idx) => (
+                      <div style={{
+                        display: "flex",
+                        justifyContent: "space-evenly",
+                        alignItems: "flex-start",
+                        flexDirection: 'column'
+                      }} key={idx}>
+                        <h6 style={{
+                          color: '#0080ff',
+                          margin: 0
+                        }}>🔹 {item.title}: </h6>
+                        <p style={{
+                          marginTop: 3
+                        }}>{item.info}</p>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            )
+          }
+
           <h2>Register to SIGHT</h2>
-          <label>Email address</label>
-          <input type="email" placeholder="name@domain.com" />
-          <label>Password</label>
-          <input type="password" placeholder="Enter your password" />
-          <label>Confirm Password</label>
-          <input type="password" placeholder="Re-enter your password" />
-          <button className='primary-button register-in-button'>Register with Email</button>
+          <form action="">
+            <label>Email address</label>
+            <input type="email" placeholder="name@domain.com" />
+            <label>Password</label>
+            <input type="password" placeholder="Create new password" />
+            <label>Confirm Password</label>
+            <input type="password" placeholder="Re-enter to confirm password" />
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start", marginTop: 10 }}>
+              <input className='check-box' type='checkbox' />
+              <label className='check-box-label'>
+                By signing up, I agree to &nbsp;
+                <span
+                  style={{
+                    textDecoration: 'underline',
+                    color: '#0080ff',
+                    cursor: 'pointer'
+                  }}
+                  onClick={() => setIsVisiblePolicy(!isVisiblePolicy)}>Terms of Use and Privacy Policy.</span>
+              </label>
+            </div>
+            <button type='submit' className='primary-button register-in-button'>Register with Email</button>
+          </form>
           <p>
             Already have an account?{' '}
             <span onClick={toggleLoginRegister}>Log in</span>.
           </p>
         </div>
-      )}
-    </div>
+      )
+      }
+    </div >
   );
 };
 
---
+++ src/components/LoginSignUpBody/TermsOfService.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        title: "Use of the Application",
+        info: "You are responsible for any activity that occurs under your account."
+    },
+    {
+        title: "Privacy",
+        info: "Your use of the SIGHT application is also governed by our Privacy Policy."
+    },
+    {
+        title: "Account Registration",
+        info: "You may need to create an account to use certain features of the application. You agree to provide accurate, current, and complete information during the registration process."
+    },
+    {
+        title: "Prohibited Activities",
+        info: "You may not access or use the SIGHT application for any purpose other than that for which we make it available."
+    },
+    {
+        title: "Termination",
+        info: "We may terminate or suspend your account and access to the SIGHT application immediately, without prior notice or liability, for any reason."
+    },
+    {
+        title: "Information We Collect",
+        info: "When you use the SIGHT application, we collect information provided by you when you sign up for an account, which may include your email address, name, mobile number and profile picture. We may also collect data about your usage of the application and the content you submit."
+    },
+    {
+        title: "Data Security",
+        info: "We take reasonable steps to protect your information from unauthorized access, disclosure, alteration, or destruction."
+    },
+    {
+        title: "How We Use Your Information",
+        info: "We use the information you provide to manage your account, identify you, and send you important updates. We may use your information to respond to your inquiries, provide customer support, and improve the SIGHT application. We will not share or sell your personal information to third parties for marketing or advertising purposes."
+    }
+]
+
+export default data;
