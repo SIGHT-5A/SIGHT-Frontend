cf6a791ee330626a80b16953ef3494d8d9d49855
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-10-17 19:40:49 +0530

    🙂 Add user avatars

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,41 @@ import { Dropdown, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink, useLocation } from 'react-router-dom';
 
+
+const userAvatars = [
+    `<i class="fa-solid fa-star"></i>`,
+    `<i class="fa-solid fa-star-of-david"></i>`,
+    `<i class="fa-solid fa-face-grin-stars"></i>`,
+    `<i class="fa-solid fa-sun"></i>`,
+    `<i class="fa-solid fa-stroopwafel"></i>`,
+    `<i class="fa-solid fa-chess-knight"></i>`,
+    `<i class="fa-solid fa-chess-king"></i>`,
+    `<i class="fa-solid fa-chess-queen"></i>`,
+    `<i class="fa-solid fa-bullseye"></i>`,
+    `<i class="fa-solid fa-heart"></i>`,
+    `<i class="fa-solid fa-bomb"></i>`,
+    `<i class="fa-solid fa-fire"></i>`,
+    `<i class="fa-solid fa-feather"></i>`,
+    `<i class="fa-solid fa-shield"></i>`,
+    `<i class="fa-solid fa-leaf"></i>`,
+    `<i class="fa-solid fa-robot"></i>`,
+    `<i class="fa-solid fa-virus"></i>`,
+    `<i class="fa-solid fa-mask"></i>`,
+    `<i class="fa-solid fa-dragon"></i>`,
+    `<i class="fa-solid fa-lightbulb"></i>`,
+    `<i class="fa-solid fa-yin-yang"></i>`,
+    `<i class="fa-solid fa-volleyball"></i>`,
+    `<i class="fa-solid fa-spider"></i>`,
+    `<i class="fa-solid fa-apple-whole"></i>`,
+    `<i class="fa-brands fa-phoenix-framework"></i>`,
+    `<i class="fa-solid fa-dove"></i>`,
+    `<i class="fa-solid fa-cat"></i>`,
+    `<i class="fa-solid fa-mug-hot"></i>`,
+    `<i class="fa-solid fa-atom"></i>`,
+    `<i class="fa-brands fa-octopus-deploy"></i>`,
+    `<i class="fa-brands fa-first-order"></i>`
+]
+
 const Navbar = () => {
     const [uncheckedNotifications, setUncheckedNotifications] = useState(10);
     const [route, setRoute] = useState("/");
@@ -18,8 +53,10 @@ const Navbar = () => {
             setRoute(location.pathname);
         };
         isActive();
+        console.log(userAvatars.length)
     }, [])
 
+    const selectedAvatar = userAvatars[29];
     return (
         <div className='my-navbar'>
             <div className='nav-logo'>
@@ -54,7 +91,11 @@ const Navbar = () => {
                 </Tippy>
                 <Dropdown>
                     <Dropdown.Toggle style={{ display: "flex", alignItems: "center", justifyItems: "center", margin: 6, background: "transparent", paddingLeft: 10, paddingRight: 10, paddingTop: 2, paddingBottom: 2 }} variant="dark">
-                        <Image className='user-avatar' src={defaultProfile} roundedCircle />
+                        <div style={{
+                            background: "white",
+                            color: "var(--blue)",
+                            fontSize: 20
+                        }} className="user-avatar" dangerouslySetInnerHTML={{ __html: selectedAvatar }} />
                     </Dropdown.Toggle>
                     <Dropdown.Menu style={{ textAlign: 'left' }} variant='dark' align="right">
                         <Dropdown.Header style={{ fontWeight: 600, fontSize: 15, color: "white" }}>My Profile</Dropdown.Header>
